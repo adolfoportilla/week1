33a9505bdf596b5ed4e2e8d2e6e6063aafb0274b
adolfo portilla <adolfoportilla@adolfo.local>
2016-01-31 20:43:00 -0600

    Here is my commit message

---
+++ MovieReviewer/DetailsViewController.swift
@@ -0,0 +1,64 @@
+//
+//  DetailsViewController.swift
+//  MovieReviewer
+//
+//  Created by adolfo portilla on 1/31/16.
+//  Copyright © 2016 Adolfo Portilla. All rights reserved.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+    
+    
+    
+    @IBOutlet weak var posterimageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var overviewLabel: UILabel!
+    
+    var movie: NSDictionary!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let title = movie["title"] as?  String
+        titleLabel.text = title
+        
+        let overview = movie["overview"]
+        overviewLabel.text = overview as? String
+        
+        if let posterPath = movie["poster_path"] as? String {
+            let posterBaseUrl = "http://image.tmdb.org/t/p/w500"
+            let imageUrl = NSURL(string: posterBaseUrl + posterPath)
+            posterimageView.setImageWithURL(imageUrl!)
+        }
+        else {
+            // No poster image. Can either set to nil (no image) or a default movie poster image
+            // that you include as an asset
+            let noimageBaseUrl = "http://www.aadprt.org/application/files/8514/4044/9309/"
+            let noimageUrl = NSURL(string: noimageBaseUrl + "no_image_available.gif")
+            posterimageView.setImageWithURL(noimageUrl!)
+        }
+        
+        print(movie)
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    
+
+}
